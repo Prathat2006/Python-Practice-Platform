0b8f994a5b43e1eaac82a6924086897aee9dfd30
Prathmesh Hatwar <121960180+Prathat2006@users.noreply.github.com>
2026-04-29 16:04:05 +0530

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
+  base: '/Python-Practice-Platform/'
   return {
     plugins: [react(), tailwindcss()],
     define: {
